ba6bd5d8e166fca014237b3a31a579c81a1eb376
Claudius Kerth <claudiuskerth@gmail.com>
2014-05-27 13:26:31 +0100

    added SSAKEoptimiser_PFM.pl and cleaned up SSAKEoptimiser_MCE.pl

--- SSAKEoptimiser_PFM.pl
+++ SSAKEoptimiser_PFM.pl
@@ -1,15 +1,12 @@
 #!/usr/bin/env perl 
 #===============================================================================
 #
-#         FILE: SSAKEoptimiser_MCE.pl
+#         FILE: SSAKEoptimiser_PFM.pl
 #
-#        USAGE: ./SSAKEoptimiser_MCE.pl  
+#        USAGE: ./SSAKEoptimiser_PFM.pl  
 #
-#  DESCRIPTION: This script runs the SSAKE assembler on an array of input files
-#  				for a range of kmer lengths and keeps the output containing the
-#  				the longest contig for each input file. It does the SSAKE runs 
-#  				in parallel with the Perl module Many-core Engine (MCE), maximising 
-#  				the usage of available cores.
+#  DESCRIPTION: This script uses Parallel:ForkManager to parallise de novo
+#  				assemblies with SSAKE.
 #
 #      OPTIONS: ---
 # REQUIREMENTS: ---
@@ -18,134 +15,86 @@
 #       AUTHOR: Claudius Kerth (CEK), c.kerth[at]sheffield.ac.uk
 # ORGANIZATION: 
 #      VERSION: 1.0
-#      CREATED: 15/05/14 12:12:18
+#      CREATED: 23/05/14 18:40:10
 #     REVISION: ---
 #===============================================================================
 
+ # :TODO      :27/05/14 13:08:18:CEK: check for required packages
+ # :TODO      :27/05/14 13:08:51:CEK: parse command line arguments for max forks and input file path
 
 use strict;
 use warnings;
 
 use Time::HiRes qw(time sleep);
 
-my $parallel = @ARGV ? shift @ARGV : 0;
-
-## always run in parallel mode, not matter
-## wether specified on command line or not
-$parallel = 1;
-
 my $start = time();
 
 #-------------------------------------------------------------------------------
 #  get input and sort by size
 #-------------------------------------------------------------------------------
-use MCE::Loop chunk_size => 1;
-
-MCE::Loop::init {
-	max_workers => 10
-};
 
 ## get input file names
 my @files = glob("input/*.fa");
 
-
+## get line count for each file
 my %h;
-# get file size for each input file
-if($parallel){
-	### in parallel using MCE::Loop ##
-	%h = mce_loop {
-		my $filename = $_;
-		my $filesize = -s $filename;
-		MCE->gather($filename, $filesize);
-	} @files;
-}else{
-	### in series ###
-	for (@files){
-		my $filename = $_;
-		my $filesize = -s $filename;
-		$h{$filename} = $filesize;
-	}
+for (@files){
+	my $filename = $_;
+	my $filesize = -s $filename;
+	$h{$filename} = $filesize;
 }
-
-## sort files by size descendingly ##
+## sort files by line count descendingly
 my @files_sorted = sort {$h{$b} <=> $h{$a}} keys %h;
 
+use Parallel::ForkManager;
+
+# initialise PFM object with maximum number of processes to fork
+my $pm = new Parallel::ForkManager(22);
 
 #-------------------------------------------------------------------------------
 #  gather function
 #-------------------------------------------------------------------------------
+my %gather;
 
-## This function is run by the manager process ##
-sub gather_output {
-	# this hash temporarily stores file names and kmer lengths
-	# of completed SSAKE runs
-	my %tmp; 
-	return sub {
-		my ($file) = @_;
-		$tmp{$file}++;
-		if($tmp{$file} == 33-11+1){
-			MCE->print("Finding longest contig for $file.\n");
+$pm->run_on_finish(
+	## this code block is run in the parent process,
+	## when a child has terminated (finished).
+	sub { 
+		## the third parameter received by the 'run_on_finish' method
+		## is the 'identification of the process'
+		## given when starting the child process
+		## see Callbacks in the documentation of Parallel::ForkManager
+		my $file = $_[2];
+		$gather{$file}++;
+		if($gather{$file}==33-11+1){
+			print("Finding longest contig for $file.\n");
 			find_longest_contig($file);
-			delete $tmp{$file};
+			delete $gather{$file};
 		}
-		return;
 	}
-}
-
+);
 
 #-------------------------------------------------------------------------------
 #  run SSAKE in parallel
 #-------------------------------------------------------------------------------
 
-use MCE::Step;
-
-## create reference to input
-my $input =  [@files_sorted[30 .. 40]]; 
-
-if($parallel){
-	## run MCE Step ##
-	mce_step {
-		task_name => ['command', 'SSAKE'],
-		max_workers => [ 1, 'Auto-2' ],
-		chunk_size => 1,
-		gather => gather_output()
-	}, \&setup_commandline, \&run_SSAKE, $input;
-}else{
-	for my $file (@{$input}){
-		for my $kmer (11..33){
+## run parallel assemblies
+for my $file (@files_sorted[30..40]){
+	for my $kmer (11..33){
+		print("Assembling $file with kmer length of ", $kmer, "\n");
+		my $pid = $pm->start($file) and next;
 			my $cmd = "SSAKE -f $file -w 1 -o 1 -m $kmer -c 1 > /dev/null";
-			print("Assembling $file with kmer length of ", $kmer, "\n");
-			# this does a fork for the command,
-			# while the worker waits for the forked process to complete
-			system($cmd) == 0 or die $!; 
-			find_longest_contig($file);
-		}
+			system($cmd) == 0  or die $!;
+			$pm->finish;
 	}
 }
+$pm->wait_all_children;
 
 printf STDERR "\n## Comnpute time: %0.3f secs\n\n", time() - $start;
-
 
 #-------------------------------------------------------------------------------
 #  subroutines
 #-------------------------------------------------------------------------------
-
-sub setup_commandline {
-	my $file = $_;
-	for my $kmer (11..33){
-		MCE->step($file, $kmer);
-	}
-}
-
-sub run_SSAKE {
-	my ($mce, $file, $kmer) = @_;
-	my $cmd = "SSAKE -f $file -w 1 -o 1 -m $kmer -c 1 > /dev/null";
-	MCE->print("Assembling $file with kmer length of ", $kmer, "\n");
-	# this does a fork for the command,
-    # while the worker waits for the forked process to complete
-	system($cmd) == 0 or die $!; 
-	MCE->gather($file);
-}
 
 #===  FUNCTION  ================================================================
 #         NAME: Find_Longest_Contig
@@ -162,8 +111,8 @@ sub run_SSAKE {
 sub find_longest_contig {
 	my $file = shift;
 
-	# get output file names containing the contigs
-	my @output = `ls $file*contigs`; # use glob
+	# get all output file names containing the contigs
+	my @output = `ls $file*contigs`; 
 	chomp @output;
 
 	my $longest_contig_length = 0;
@@ -171,10 +120,10 @@ sub find_longest_contig {
 
 	# get the length of the longest contig for each SSAKE x kmer run
 	for my $output (@output){
-	
+
 		if(-z $output){ # if the contig file is empty
+			print("Contig file is empty for $output\n");
 			$output =~ s/contigs$/\*/; 
-			MCE->print("Contig file is empty for $output\n");
 			system("rm -f $output")==0 or die $!;
 			next; 
 		}
@@ -188,9 +137,9 @@ sub find_longest_contig {
 			$longest_contig_length = $length if ($length > $longest_contig_length);
 		}
 		close $IN;
-		MCE->print("$output : $longest_contig_length\n");
+		print("$output : $longest_contig_length\n");
 		unless ($longest_contig_length){
-			MCE->print("Found no contig length for $output\n");
+			print("Found no contig length for $output\n");
 			next;
 		}; 
 		$longest_contig_hash{$output} = $longest_contig_length;
@@ -198,7 +147,7 @@ sub find_longest_contig {
 
 	# exit if no contigs could be assembled
 	unless(keys %longest_contig_hash){
-		MCE->print("No contigs could be assembled for $file.\n");
+		print("No contigs could be assembled for $file.\n");
 		map { s/contigs$/\*/ } @output;
 		system("rm -f @output")==0 or die $!;
 		return;
@@ -214,7 +163,7 @@ sub find_longest_contig {
 	# save the name of the output file containing the longest contig from
 	# all SSAKE runs
 	my $keep = shift(@remove);
-	MCE->print("File containing longest contig:\n", "$keep : $longest_contig_hash{$keep}\n\n");
+	print("File containing longest contig:\n", "$keep : $longest_contig_hash{$keep}\n\n");
 
 
 	map { s/contigs$/\*/ } @remove;
